e0a9ac4cad9b636f9e148d059bb8497e5cb2e5f0
gerardo2600 <73725618+gerardo2600@users.noreply.github.com>
2021-03-19 10:55:45 -0600

    Add files via upload

---
+++ Model/horario.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+const p = require('path');
+const Path = p.join(require('os').homedir(),"HorarioDB/horario.json");
+
+if (!fs.existsSync(p.join(require('os').homedir(),"HorarioDB"))){
+    fs.mkdirSync(p.join(require('os').homedir(),"HorarioDB"));
+}
+
+let horario = {
+    R:
+    ()=>{
+        let data;
+        try{
+            data = fs.readFileSync(Path);
+            data = JSON.parse(data);
+        }
+        catch(er){
+            fs.writeFileSync(Path,"[[],[],[],[],[],[],[]]");
+            data = undefined;
+            console.log(" e1: "+er);
+        }
+        return data;
+    },
+
+    U: 
+        (dia,entrada,clase,hi,hf)=>{
+            try{
+                data = fs.readFileSync(Path);
+                data = JSON.parse(data);
+                data[dia][entrada].id = clase;
+                data[dia][entrada].hi = hi;
+                data[dia][entrada].hf = hf;
+                data = JSON.stringify(data);
+                fs.writeFileSync(Path,data); 
+            }
+            catch(er){
+                console.log(" e1: "+er);
+            }
+        },
+    C: 
+        (dia,clase,hi,hf)=>{
+            try{
+                data = fs.readFileSync(Path);
+                data = JSON.parse(data);
+                data[dia].push({id:clase,hi:hi,hf:hf});
+                data = JSON.stringify(data);
+                fs.writeFileSync(Path,data);
+            }
+            catch(er){
+                //fs.writeFileSync(Path,"[[],[],[],[],[],[]]");
+                console.log(" e1: "+er);
+            }
+        },
+    D: 
+        (d,e)=>{
+            try{
+                data = fs.readFileSync(Path);
+                data = JSON.parse(data);
+                data[d].splice(e,1);
+                data = JSON.stringify(data);
+                fs.writeFileSync(Path,data);
+            }
+            catch(er){
+
+                console.log(" e1: "+er);
+            }
+        },
+};
+
+module.exports = horario;
